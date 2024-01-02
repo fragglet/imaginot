16f41b33a407fea2c4314d875b8fdfc120815dba
Simon Howard <fraggle@soulsphere.org>
2024-01-01 23:23:59 -0500

    Add initial sector read hooks.

--- imaginot.c
+++ imaginot.c
@@ -4,6 +4,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+
+#pragma pack(push, 1)
+
+struct bpb {
+	uint16_t bytes_per_sector;
+	uint8_t sectors_per_cluster;
+	uint16_t reserved_sectors;
+	uint8_t num_fats;
+	uint16_t root_dir_count;
+	uint16_t total_sectors;
+	uint8_t media_descriptor;
+	uint16_t sectors_per_fat;
+};
 
 static void (interrupt far *old_int13)();
 static void (interrupt far *old_int21)();
@@ -15,11 +30,97 @@ static int int13_count = 0;
 static int int21_count = 0;
 static int int25_count = 0;
 static int int26_count = 0;
+static int reads = 0;
+
+//
+// Emulated floppy disk:
+// Imaginet worked by sharing a floppy drive at the raw, hardware level.
+// Sopwith players exchanged data by writing to a particular sector on
+// the disk. So we emulate a simple DOS drive and barebones FAT12:
+//
+//
+enum {
+	//            Sector #   Purpose
+	SECTOR_BOOT,      // 0   Boot sector & BIOS parameter block (BPB)
+	SECTOR_FAT,       // 1   File allocation table
+	SECTOR_DIRECTORY, // 2   Directory table for root directory (B:\)
+	SECTOR_SOPWITH1,  // 3   Contents of file SOPWITH1.DTA
+	SECTOR_SEMAPHOR,  // 4   Contents of file SEMAPHOR
+};
+
+static uint8_t ReadBootSector(void far *data)
+{
+	struct bpb far *bpb;
+
+	bpb = ((struct bpb far *) data) + 0x0b;
+
+	bpb->bytes_per_sector = 512;
+	bpb->sectors_per_cluster = 1;
+	bpb->reserved_sectors = 0;
+	bpb->num_fats = 1;
+	bpb->root_dir_count = 2;
+	bpb->total_sectors = 320;  // 160KiB disk
+	bpb->media_descriptor = 0;
+	bpb->sectors_per_fat = 1;
+
+	return 0;
+}
+
+static uint8_t ReadDirectory(void far *data)
+{
+	return 0;
+}
+
+static uint8_t ReadSector(void far *data, uint32_t sector, uint16_t cnt)
+{
+	// Only ever one sector:
+	if (cnt != 1) {
+		return 1;
+	}
+
+	// All sectors are low:
+	if (sector > 16) {
+		return 1;
+	}
+
+	++reads;
+
+	_fmemset(data, 0, 512);
+
+	switch (sector) {
+	case SECTOR_BOOT:
+		return ReadBootSector(data);
+
+	case SECTOR_FAT:
+		// Unused.
+		return 0;
+
+	case SECTOR_DIRECTORY:
+		return ReadDirectory(data);
+
+	case SECTOR_SOPWITH1:
+	case SECTOR_SEMAPHOR:
+		return 1;  // TODO
+
+	default:
+		return 1;
+	}
+}
 
 // Int 13h: BIOS interrupt call
 static void interrupt far Int13(union INTPACK ip)
 {
 	++int13_count;
+
+	// ah=2 -> DISK - READ SECTOR(S) INTO MEMORY
+	// al=1 -> Drive B:
+	if (ip.h.ah == 2 && ip.h.dl == 1) {
+		uint32_t sector = ((uint32_t) ip.x.cx)
+			| (((uint32_t) ip.h.dh) << 16);
+		uint8_t result = ReadSector(
+			MK_FP(ip.x.es, ip.x.bx), sector, ip.h.al);
+		return;
+	}
 	_chain_intr(old_int13);
 }
 
@@ -34,6 +135,14 @@ static void interrupt far Int21(union INTPACK ip)
 static void interrupt far Int25(union INTPACK ip)
 {
 	++int25_count;
+
+	// al=1 -> Drive B:
+	if (ip.h.al == 1) {
+		uint8_t result = ReadSector(
+			MK_FP(ip.x.ds, ip.x.bx), ip.x.dx, ip.x.cx);
+		return;
+	}
+
 	_chain_intr(old_int25);
 }
 
@@ -57,6 +166,7 @@ static void RestoreInterrupts(void)
 		printf(" int 21 calls: %7d\n", int21_count);
 		printf(" int 25 calls: %7d\n", int25_count);
 		printf(" int 26 calls: %7d\n", int26_count);
+		printf(" reads:        %7d\n", reads);
 	}
 }
 
