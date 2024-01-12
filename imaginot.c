8662bc0871dcfd701834321fb1679ced65742b33
Simon Howard <fraggle@soulsphere.org>
2024-01-11 19:50:03 -0500

    Use flags library and save -net memory address.

--- imaginot.c
+++ imaginot.c
@@ -2,12 +2,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <process.h>
 
 #include "fakedisk.h"
+#include "lib/flag.h"
+#include "lib/log.h"
+
+static long doomcom_addr = 0;
+
+void SetDoomcomAddr(long value)
+{
+	doomcom_addr = value;
+}
 
 int main(int argc, char *argv[])
 {
+	char **args;
+
+	SetHelpText("Sopwith multiplayer adapter", "ipxsetup %s sopwith2");
+	APIPointerFlag("-net", SetDoomcomAddr);
+
+	args = ParseCommandLine(argc, argv);
+	if (args == NULL)
+	{
+		ErrorPrintUsage("");
+	}
+	if (doomcom_addr == 0)
+	{
+		ErrorPrintUsage("You should run this program via a driver, eg. "
+		                "IPXSETUP.");
+	}
+
 	HookDiskInterrupts();
 	atexit(RestoreDiskInterrupts);
-	return system("command");
+	return spawnv(P_WAIT, args[0], (void *) args);
 }
