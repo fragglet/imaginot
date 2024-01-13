fe0704b38bd0f6c5ea82fea0dff269a773f4ace4
Simon Howard <fraggle@soulsphere.org>
2024-01-13 14:01:05 -0500

    Add GPL header.

--- doomnet.c
+++ doomnet.c
@@ -1,6 +1,6 @@
 //
 // Copyright(C) 1993 id Software, Inc.
-// Copyright(C) 2019-2023 Simon Howard
+// Copyright(C) 2019-2024 Simon Howard
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
