9a1e4c1e074bf55c3a414c8d33ce9c6966df5be3
Simon Howard <fraggle@soulsphere.org>
2024-01-15 21:10:34 -0500

    Delete int 21h hook.
    Turned out this was not needed.

--- fakedisk.c
+++ fakedisk.c
@@ -88,7 +88,6 @@ struct sop_multio
 };
 
 static void (interrupt far *old_int13)();
-static void (interrupt far *old_int21)();
 static void (interrupt far *old_int25)();
 static void (interrupt far *old_int26)();
 
@@ -96,7 +95,6 @@ static bool locked = false;
 
 static bool hooked = false;
 static int int13_count = 0;
-static int int21_count = 0;
 static int int25_count = 0;
 static int int26_count = 0;
 static int reads = 0, writes = 0;
@@ -396,13 +394,6 @@ static void interrupt far Int13(union INTPACK ip)
     _chain_intr(old_int13);
 }
 
-// Int 21h: main DOS API.
-static void interrupt far Int21(union INTPACK ip)
-{
-    ++int21_count;
-    _chain_intr(old_int21);
-}
-
 // DOS interrupts 25h and 26h leave the FLAGS register on the stack and
 // it is the job of the caller to clean it up. So here we have a special
 // modified version of the Watcom interrupt return sequence that does a
@@ -493,13 +484,11 @@ void RestoreDiskInterrupts(void)
     if (hooked)
     {
         _dos_setvect(0x13, old_int13);
-        _dos_setvect(0x21, old_int21);
         _dos_setvect(0x25, old_int25);
         _dos_setvect(0x26, old_int26);
         hooked = false;
         printf("Normal interrupts restored.\n");
         printf(" int 13 calls: %7d\n", int13_count);
-        printf(" int 21 calls: %7d\n", int21_count);
         printf(" int 25 calls: %7d\n", int25_count);
         printf(" int 26 calls: %7d\n", int26_count);
         printf(" reads:        %7d\n", reads);
@@ -513,8 +502,6 @@ void HookDiskInterrupts(void)
     {
         old_int13 = _dos_getvect(0x13);
         _dos_setvect(0x13, Int13);
-        old_int21 = _dos_getvect(0x21);
-        _dos_setvect(0x21, Int21);
         old_int25 = _dos_getvect(0x25);
         _dos_setvect(0x25, Int25);
         old_int26 = _dos_getvect(0x26);
